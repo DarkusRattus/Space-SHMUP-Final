869da24a309bb6f14b1a402a37cdfdd0e6ec7641
DarkusRattus <kristian.benson@rutgers.edu>
2016-12-08 11:55:18 -0500

    Revert "Testing"
    This reverts commit 9acd78ecedd1493b8fd711db525c500ccf1d9abb.

--- Assets/_Scripts/Main.cs
+++ Assets/_Scripts/Main.cs
@@ -16,8 +16,7 @@ public class Main : MonoBehaviour {
     public WeaponType[] powerUpFrequency = new WeaponType[] { WeaponType.blaster,
                                                               WeaponType.blaster,
                                                               WeaponType.spread,
-                                                              WeaponType.bomb,
-                                                              WeaponType.shield};
+                                                              WeaponType.shield     };
 
     public bool ___________;
 
